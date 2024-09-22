31770cc359dfe18d5f0e7d1f2b4fecef67821441
Simon <blaineighteen@gmail.com>
2024-09-23 02:02:11 +0800

    add jest and first test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,19 @@
+import { classNames } from "./classNames"
+
+describe('classNames', () => {
+    test('test 1', () => {
+        expect(classNames('someClass')).toBe('someClass')
+    })
+
+    test('test 2', () => {
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe('someClass class1 class2')
+    })
+
+    test('test 3', () => {
+        expect(classNames('someClass', {hovered: true, scrollable:true}, ['class1', 'class2'])).toBe('someClass class1 class2 hovered scrollable')
+    })
+
+    test('test 4', () => {
+        expect(classNames('someClass', {hovered: true, scrollable:false}, ['class1', 'class2'])).toBe('someClass class1 class2 hovered')
+    })
+})
