47e43c9c69567014b015ba007333ef9fb7506b06
Simon <blaineighteen@gmail.com>
2024-09-25 16:40:14 +0800

    yml fixed

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,18 +4,21 @@ import { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+
 interface SidebarProps {
     className?: string;
 }
 export const Sidebar = ({className}: SidebarProps) => {
-    const [ collapsed, setCollapsed ] = useState(false)
+    const { t } = useTranslation()
+    const [ collapsed, setCollapsed] = useState(false)
     const onToggle = () => {
         setCollapsed((prev) => !prev)
     }
     return (
         <div data-testid="sidebar" className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}>
             <Button data-testid="sidebar-toggle" onClick={onToggle}>
-                toggle
+                {t('toggle')}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
