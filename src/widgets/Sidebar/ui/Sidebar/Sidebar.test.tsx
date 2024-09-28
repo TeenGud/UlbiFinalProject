e1d856429a709a07c8465595ac090d65d0f4b4ad
Simon <blaineighteen@gmail.com>
2024-09-28 21:10:59 +0800

    fixing yml

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -2,17 +2,18 @@ import { fireEvent, screen } from '@testing-library/react';
 import { withTranslation } from 'react-i18next';
 import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
 import { Sidebar } from './Sidebar';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('Sidebar', () => {
     test('test 1', () => {
         const SidebarWithTranslation = withTranslation('ns')(Sidebar);
-        renderWithTranslation(<SidebarWithTranslation />);
+        componentRender(<SidebarWithTranslation />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('test toggle', () => {
         const SidebarWithTranslation = withTranslation('ns')(Sidebar);
-        renderWithTranslation(<SidebarWithTranslation />);
+        componentRender(<SidebarWithTranslation />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
